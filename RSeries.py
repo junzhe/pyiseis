94d86962f381e6a3fc0ed34bbcab080831ccb0bc
junzhe <junzhe@umich.edu>
2013-03-02 18:16:09 -0500

    Modify RSeries.py

---
+++ RSeries.py
@@ -0,0 +1,10 @@
+import numpy as np
+def RSeries(p,k):
+    r = np.zeros((p.size, p.size));
+    for p in range(p[2].size):
+        r[:l] = np.prod(title(p[:l],(1,k+1))-title(np.zeros(k), (p[1].size, 1)),2)
+    return r
+
+                       
+              
+
